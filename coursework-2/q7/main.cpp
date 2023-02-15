fe1e0d6ef0c56f31ab7a00145fc881c03240e799
Max-Ded <Dedieu.mx@gmail.com>
2023-02-15 19:28:06 +0000

    q8 fix BSM

--- coursework-2/q7/main.cpp
+++ coursework-2/q7/main.cpp
@@ -3,32 +3,49 @@
 
 using namespace std;
 
-double compute_d1(double St,double K, double r,double sigma,double t){
-    return (log(St/K) + (r+sigma*sigma/2)*t)/(sigma*t);
+double compute_d1(double S,double K, double r,double sigma,double t){
+    /// @brief Compute the d1 factor of the BSM model
+    /// @param S Spot price
+    /// @param K Strike price
+    /// @param r interest rate
+    /// @param sigma volatility
+    /// @param t time to expiration
+    /// @return d1
+    return (log(S/K) + (r+sigma*sigma/2)*t)/(sigma*sqrt(t));
 }
 
 double N(double x){
+    /// @brief Compute the CDF of a N(0,1) normal law
+    /// @param x positive real to evalute N
+    /// @return N(x)
     return 0.5 + 0.5 * erf(x / sqrt(2.0));
 }
-double compute_c(double St,double K, double r, double sigma,double t){
-    double d1 = compute_d1(St,K,r,sigma,t);
+double compute_c(double S,double K, double r, double sigma,double t){
+    /// @brief Compute the BSM price of a call
+    /// @param S Spot price
+    /// @param K Strike price
+    /// @param r interest rate
+    /// @param sigma volatility
+    /// @param t time to expiration
+    /// @return Call price
+    double d1 = compute_d1(S,K,r,sigma,t);
     double d2 = d1 - sigma * sqrt(t);
 
-    return N(d1)*St - N(d2) * K * exp(-r*t);
+    return N(d1)*S - N(d2) * K * exp(-r*t);
 }
 
 int main(){
 
-    double t = 1/4;
+    double t = 0.25;
     double K = 95;
-    double St = 100;
-    double r = 10;
-    double sigma = 50;
+    double S = 100;
+    double r = 0.1;
+    double sigma = 0.5;
 
-    double C = compute_c(St,K,r,sigma,t);
+    double C = compute_c(S,K,r,sigma,t);
 
     cout << "Parameters : " << endl  << "t = " << t << endl << "K = " << K
-         << endl << "St = " << St << endl << "r = " << r << endl << "sigma = "
+         << endl << "S = " << S << endl << "r = " << r << endl << "sigma = "
          << sigma << endl << endl << "C = " << C << endl; 
 
     return 0;
--- coursework-2/q8/main.cpp
+++ coursework-2/q8/main.cpp
@@ -2,60 +2,97 @@
 #include <math.h>
 using namespace std;
 
-double compute_d1(double St,double K, double r,double sigma,double t){
-    return (log(St/K) + (r+sigma*sigma/2)*t)/(sigma*t);
+double compute_d1(double S,double K, double r,double sigma,double t){
+    /// @brief Compute the d1 factor of the BSM model
+    /// @param S Spot price
+    /// @param K Strike price
+    /// @param r interest rate
+    /// @param sigma volatility
+    /// @param t time to expiration
+    /// @return d1
+    return (log(S/K) + (r+sigma*sigma/2)*t)/(sigma*sqrt(t));
 }
 
 double N(double x){
+    /// @brief Compute the CDF of a N(0,1) normal law
+    /// @param x positive real to evalute N
+    /// @return N(x)
     return 0.5 + 0.5 * erf(x / sqrt(2.0));
 }
 
-class BlackScholesCallClass{
-    public : 
-        double s;
+class BlackScholesCallFunctor{
+    private : 
+        double T;
+        double K;
+        double S;
+        double r;
     public :
-        double BlackScholesCallFunctor(double T,double K,double S,double r);
+        BlackScholesCallFunctor(double T,double K,double S,double r);
         double operator()(double sigma) const;
+};
+
+double BlackScholesCallFunctor :: operator()(double sigma) const{
+    /// @brief Compute the call price with the class attribute and parameter sigma
+    /// @param sigma volatility
+    /// @return call price
+    double d1 = compute_d1(S,K,r,sigma,T);
+    double d2 = d1 - sigma * sqrt(T);
+    double C = N(d1)*S - N(d2) * K * exp(-r*T);
+    return C;
 }
 
-double BlackScholesCallClass :: operator()double sigma) const{
-    this.s = sigma;
-}
-double BlackScholesCallClass :: BlackScholesCallFunctor(double T,double K,double S,double r){
-    double d1 = compute_d1(S,K,r,s,T);
-    double d2 = d1 - s * sqrt(T);
-
-    return N(d1)*S - N(d2) * K * exp(-r*t);
-}
+BlackScholesCallFunctor :: BlackScholesCallFunctor(double T,double K,double S,double r) : T(T), K(K), S(S) ,r(r){}
 
 template <typename T>
 double interval_bisection(double y_target ,double left,double right,double epsilon,T functor){
-    double midpoint = (right+left) / 2;
-    double T_midpoint = functor(midpoint);
+    /// @brief Find the pre-image of y_target w.r.t f (with epsilon precision) in a search interval
+    /// @tparam T return type of the functor
+    /// @param y_target point which we want ot find the pre-image of w.r.t f
+    /// @param left left bound of the search interval
+    /// @param right right bound of the search interval
+    /// @param epsilon precision (functions stops when |f(x_target)-y_target| < epsilon)
+    /// @param functor function f to evaluate, returns a type T
+    /// @return pre-image of y_target
+
+    double midpoint = (right+left) / 2; // compute center of the search imperial
+    double T_midpoint = functor(midpoint); //
+
     while (abs(T_midpoint-y_target)>epsilon){
+        /// Iterate until convergence is reached (|f(x_target)-y_target| < epsilon)
         if (T_midpoint < y_target){
+            // if midpoint is below y_target, move the left bound
             left=midpoint;
         }
         else{
+            // if midpoint is above y_target, move the right bound
             right = midpoint;
         }
-        midpoint = (right+left) / 2;
-        T_midpoint = functor(midpoint);
+        midpoint = (right+left) / 2; // re-compute midpoint
+        T_midpoint = functor(midpoint); // re-compute f(midpoint)
         }
     return midpoint;
 }
 
-double functor(double x){
-    return x-5;
-}
-
 int main(){
-    double y_target = 0;
-    double left = -10;
-    double right = 10;
-    double epsilon = 1;
+    double T = 0.25;
+    double K = 95;
+    double S = 100;
+    double r = 0.1;
+    double C_target = 13.6953;
 
-    double res = interval_bisection(y_target,left,right,epsilon,functor);
-    cout << "Res is : " << res <<endl;
+    double left = 0;
+    double right = 0.9;
+    double epsilon = 0.001;
+
+    cout << "Parameters : " << endl  << "T = " << T << endl << "K = " << K
+         << endl << "S = " << S << endl << "r = " << r << endl << "C_target = "
+         << C_target << endl << "Looking in the interval : [" << left << "," 
+         << right << "] (precision : " << epsilon << ")" << endl << endl;
+
+    BlackScholesCallFunctor functor = BlackScholesCallFunctor(T,K,S,r);
+
+    double sigma = interval_bisection(C_target,left,right,epsilon,functor);
+    cout << "Result : "<< endl << "Sigma = " << sigma <<endl;
+
     return 0;
 }
